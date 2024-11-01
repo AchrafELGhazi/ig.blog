f693f9ba6790fcecdeeefc6ec6e8c481ba9c3468
Achraf1001 <160622699+Achraf1001@users.noreply.github.com>
2024-11-01 22:42:15 +0100

    creation of the vite+react app

---
+++ blog app management/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(12)
+
+  return (
+    <>
+      <h1>IKRAM GHALA</h1>
+    </>
+  )
+}
+
+export default App
