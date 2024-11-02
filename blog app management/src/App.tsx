3a193eb3c42883c6489163da141193b4b7b7c936
Achraf1001 <160622699+Achraf1001@users.noreply.github.com>
2024-11-02 02:24:30 +0100

    test

--- blog app management/src/App.tsx
+++ blog app management/src/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(12)
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const person = { name:'yoshi', age:30 };
+  
 
   return (
-    <>
-      <h1 className='text-9xl font-bold'>IKRAM GHALA</h1>
-    </>
+    <div className='w-full h-screen m-0 bg-gray-300'>    
+      <h1 className=' text-center p-10 text-9xl font-poppins-700 '>IKRAM GHALA</h1>
+      <h1 className='text-7xl font-semibold m-10 text-center p-0   '>{title}</h1>
+      <p className='text-5xl font-light m-10 text-center p-0 '> Ikram ghala is thinking of suicide, of ending it all</p>
+      <p className='text-5xl font-light m-10 text-center p-0 '>Liked {likes} times</p>
+    </div>
   )
 }
 
